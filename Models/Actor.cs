06830671f046390d051db50b97d9da4b26ef75e1
Muhammad Farhan Ali <farhanali8877@gmail.com>
2022-10-18 23:32:16 +0500

    Add project files.

---
+++ Models/Actor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace etickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        // This id for table of actor in database
+        public int ID { get; set; }
+        public string ProfilePictrueURL { get; set; }
+        public string FullName { get; set; }
+        public string BioData { get; set; }
+    }
+}
---
+++ Models/Cinema.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace etickets.Models
+{
+    public class Cinema
+    {
+        [Key]
+        // This id for table of actor in database
+        public int ID { get; set; }
+        public string CinemaLogo { get; set; }
+        public string Name { get; set; }
+        public string Descriptions { get; set; }
+    }
+}
